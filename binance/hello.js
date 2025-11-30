6a84492c2047b97a583e9956dd54cd44d7d6447e
banner2007 <47283735+banner2007@users.noreply.github.com>
2025-11-29 21:18:10 -0500

    Delete binance directory

--- binance/hello.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ msg: "Hola desde el backend Render" });
-});
-
-export default router;
