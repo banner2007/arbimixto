736c8214a827a0aea2a0d478716e8cd6403214ca
banner2007 <47283735+banner2007@users.noreply.github.com>
2026-01-02 18:30:11 -0500

    Delete config directory

--- config/env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const config = {
-  binanceApiKey: process.env.BINANCE_API_KEY,
-  binanceSecretKey: process.env.BINANCE_SECRET_KEY,
-  bitbexApiKey: process.env.BITBEX_API_KEY,
-  bitbexSecretKey: process.env.BITBEX_SECRET_KEY
-};
